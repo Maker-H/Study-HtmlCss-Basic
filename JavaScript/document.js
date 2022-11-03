b11ff4016dc198e3a1cbf7fdb2bf7c36d3b44349
Heesom <heesom.hs@gmail.com>
2022-11-03 10:48:37 +0900

    Create .gitignore

---
+++ JavaScript/document.js
@@ -0,0 +1,13 @@
+// console.log( document.querySelector('p'))
+// console.log(document.querySelector('h1'))
+// console.log(document.querySelector('#text'))
+const paragraph = document.querySelector('.paragraph')
+
+const text = document.getElementById('text')
+
+console.log(paragraph.textContent)
+
+text.textContent = 'changed content'
+
+
+
--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,3 +1,13 @@
 // window.alert('hello')
 window.console.log('진희솜');
 console.log('penguin');
+console.clear()
+
+let name = 'heesom'
+console.log(name)
+console.clear()
+
+const promptMessage = 'enter your name'
+const data = `this is your name : ${prompt(promptMessage)}`
+console.log(data)
+
